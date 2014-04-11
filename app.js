074bf6d86cb9bb73f3d65b6d8bf70c99f2afaf09
Michael Puckett <michaelcpuckett@gmail.com>
2014-04-10 18:22:17 -0700

    Converted to promises

--- app.js
+++ app.js
@@ -1,43 +1,56 @@
 var spotify = require('spotify-node-applescript'),
-    http = require('http');
+    http = require('http'),
+    Promise = require('es6-promise').Promise;
 
-function getSong(query, callback) {
-    var searchData = '';
+function getSearchResults(query) {
+    return new Promise(function(resolve, reject) {
+        var SPOTIFY_HOST_URL = 'ws.spotify.com',
+            SPOTIFY_SEARCH_URL = '/search/1/track.json?q=',
+            data = '';
 
-    http.get({
-        host: 'ws.spotify.com',
-        port: 80,
-        path: '/search/1/track.json?q=' + query
-    }, function (res) {
-        res.on('data', function (chunk) {
-            searchData += chunk;
-        }).on('end', function () {
-            searchData = JSON.parse(searchData);
-
-            if (Array.isArray(searchData.tracks)) {
-                var artists = '';
-                for (var i = 0; i < searchData.tracks[0].artists.length; i += 1) {
-                    artists += searchData.tracks[0].artists[i].name + ', ';
+        http.get({
+            host: SPOTIFY_HOST_URL,
+            port: 80,
+            path: SPOTIFY_SEARCH_URL + query
+        }, function (res) {
+            res.on('data', function (chunk) {
+                data += chunk;
+            }).on('end', function () {
+                try {
+                    resolve(JSON.parse(data));
+                } catch (e) {
+                    reject(e, data);
                 }
-                artists = artists.substr(0, artists.length - 2);
-                console.log('Playing: ' + artists + ' - ' + searchData.tracks[0].name);
-
-                callback(null, searchData.tracks[0].href);
-            } else {
-                console.log('No results, sucker.');
-            }
+            });
+        }).on('error', function (e) {
+            reject(callback(e));
         });
-    }).on('error', function (e) {
-        console.log("Got error: " + e.message);
-        callback(e.message);
     });
+}
+
+function processSearchData(searchData) {
+    if (Array.isArray(searchData.tracks)) {
+        var artists = '';
+        for (var i = 0; i < searchData.tracks[0].artists.length; i += 1) {
+            artists += searchData.tracks[0].artists[i].name + ', ';
+        }
+        artists = artists.substr(0, artists.length - 2);
+
+        console.log('Playing: ' + artists + ' - ' + searchData.tracks[0].name);
+
+        return searchData.tracks[0].href;
+    } else {
+        console.log('No results, sucker.');
+    }
+}
+
+function handleError(error) {
+    console.trace("ERROR", e.message || e);
 }
 
 if (process.argv[2] === 'play' && process.argv[3] && process.argv[3].length > 0) {
     var query = process.argv.splice(3).join('+').toLowerCase();
-    getSong(query, function (error, songUri) {
-        spotify.playTrack(songUri);
-    });
+    getSearchResults(query).then(processSearchData, handleError).then(spotify.playTrack);
 } else {
     spotify[process.argv[2]]();
 }
